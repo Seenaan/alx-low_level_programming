98c0fd98aac35dea4c4076c78cff720a0891e4dd
sena <senanegesse@gmail.com>
2022-10-24 13:50:45 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (8str != '\0')
+	{
+		_putchar(8str++);
+	}
+	_putchar('\n');
+}
