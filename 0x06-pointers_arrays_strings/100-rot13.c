7f4c705b97ec32f5b0a54b68338fa66e432fc13e
sena <senanegesse@gmail.com>
2022-10-27 09:14:10 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string usinf rot13
+ * @str: the string targeted
+ * Return: returns the encoded string
+ */
+
+char *rot13(char *str)
+{
+	int index1, index2;
+
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13key[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (str[++index1])
+	{
+		for (index2 = 0; index2 < 52; index2++)
+		{
+			if (str[index1] == alphabet[index2])
+			{
+				str[index1] = rot13key[index2];
+				break;
+			}
+		}
+	}
+	return (str);
+}
