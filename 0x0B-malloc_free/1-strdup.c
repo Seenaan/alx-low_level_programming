31ebc3ad14edcd05201c60c6afbd4d2f74d0adcf
sena <senanegesse@gmail.com>
2022-11-09 15:23:10 -0800

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,13 +18,13 @@ char *_strdup(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	strout = (char *)malloc(sizeof(char) * (i + 1);
+	strout = (char *)malloc(sizeof(char) * (i + 1));
 
-			if (strout == NULL)
-			return (NULL);
+	if (strout == NULL)
+		return (NULL);
 
-			for (j = 0; j <= i; j++)
-			strout[j] = str[j];
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
 
-			return (strout);
-			}
+	return (strout);
+}
