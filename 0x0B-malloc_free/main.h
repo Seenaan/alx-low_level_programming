ba9e6ae20905809d1077eb29525f8d3d4ba6dfc8
sena <senanegesse@gmail.com>
2022-11-09 16:25:06 -0800

    task 5

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,9 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 int _putchar(char c);
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
+int number(char *str);
+void free_everything(char **string, int i);
 
 #endif
